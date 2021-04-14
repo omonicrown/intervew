445e0abf1d7cd52d963ba3db3fc01414872d41d7
omolade samuel <samuelfemi85@gmail.com>
2021-04-14 10:52:48 -0930

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,43 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import createPackage from './Components/createPackage';
+//import listPackage from './Components/listPackage';
+import updatePackage from './Components/updatePackage';
+import {Layout} from './Components/layout';
+import {NavigationBar} from './Components/navbar';
+import {SideNav} from './Components/sidenav';
+import Dashboard from './Components/dashboard';
+import PackageList from './Components/packageList';
+import Subscription from './Components/subscription';
+import CreateSubscription from './Components/createSubscription';
+import {GLobalProvider} from './context/globalState';
+import UpdateSubscription from './Components/updateSubscription';
+
+ 
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <GLobalProvider>
+    <BrowserRouter>
+  <NavigationBar />
+  <SideNav />
+  <Switch>
+    <Route exact path='/' component={Dashboard}></Route>
+    <Route path='/create' component={createPackage}></Route>
+    <Route path='/subscription' component={Subscription}></Route>
+    <Route path='/createsub' component={CreateSubscription}></Route>
+    <Route path='/show' component={PackageList}></Route>
+    <Route path='/edit' component={updatePackage}></Route>
+    <Route path='/editsub' component={UpdateSubscription}></Route>
+    <Route  component={404}></Route>
+    </Switch>
+   
+  </BrowserRouter>
+  </GLobalProvider>
+  ,
   document.getElementById('root')
 );
 
